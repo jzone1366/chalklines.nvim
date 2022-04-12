03a48e5b3deac7b2468a86221255a67580932b0f
Joshua Zone <joshua.zone@playvs.com>
2022-04-11 21:38:00 -0400

    Update to make Light mode more ledgible. Close 1.

--- lua/chalklines/palette.lua
+++ lua/chalklines/palette.lua
@@ -21,33 +21,35 @@ local variants = {
     hl_high = '#474747', -- highlight_high
 
     orange  = '#F34B00',
-    white   = '#C2C8D7',
+    white   = '#E7E7E7',
+
     opacity = 0.1,
     none = 'NONE',
   },
-  -- TODO: UPDATE WITH BRIGHT COLORS (Light Mode)
+
   bright = {
-    base    = '#191919', -- base
-    surface = '#242424', -- surface
-    overlay = '#303030', -- overlay
+    base    = '#E7E7E7', -- base
+    surface = '#DBDBDB', -- surface
+    overlay = '#CFCFCF', -- overlay
 
-    muted   = '#525A69', -- muted
+    muted   = '#C1C7D6', -- muted
     subtle  = '#727A8C', -- subtle
-    text    = '#C1C7D6', -- text
+    text    = '#525A69', -- text
 
-    blue    = '#76D5FF', -- foam
-    cyan    = '#6CBFB5', -- rose
-    green   = '#B7E876', -- pine
+    blue    = '#00A4EB', -- foam
+    cyan    = '#4EB1A6', -- rose
+    green   = '#70AE1E', -- pine
     magenta = '#BA76E7', -- iris
     red     = '#FF355B', -- love
-    yellow  = '#FFC251', -- gold
+    yellow  = '#D68B00', -- gold
 
-    hl_low  = '#212121', -- highlight_low
-    hl_med  = '#333333', -- highlight_med
-    hl_high = '#474747', -- highlight_high
+    hl_low  = '#DEDEDE', -- highlight_low
+    hl_med  = '#CBCBCB', -- highlight_med
+    hl_high = '#B7B7B7', -- highlight_high
 
     orange  = '#F34B00',
-    white   = '#C2C8D7',
+    white   = '#181818',
+
     opacity = 0.1,
     none = 'NONE',
   }
